b8db2c4bd4b37c50b4f41414151fd76fdbb83937
Brett M <whohat@gmail.com>
2015-04-16 15:43:53 -0700

    Created Finder class

--- find_links.py
+++ find_links.py
@@ -1,74 +1,98 @@
+#!/usr/bin/env python
+
 from sys import argv
 import requests
 import bs4
 from urlparse import urlparse
 
 
-def get_html_string(url):
-    # returns url's html as string
-    data = requests.get(url)
-    return data.text
+class Finder(object):
+    """Finds all internal links on all pages within a domain."""
+
+    def __init__(self, url):
+        self.url = str(url)
+        self.parsed_url = self.parse_url(url)
+        self.domain = '.'.join(self.parsed_url.netloc.split('.')[-2:])
+        self.links = self.find_all_links()
+
+    def __len__(self):
+        return len(self.links)
+
+    @staticmethod
+    def parse_url(base_url):
+        # used to extract the scheme, subdomain, and domain from the url
+        parsed_url = urlparse(base_url)
+        return parsed_url
+
+    def find_all_links(self):
+        # finds all internal links in the whole domain
+        links_to_visit = self.search_page(self.url)
+
+        # keeps track of all urls that have been searched
+        visited_urls = set()
+
+        # search links recursively until all links have been searched
+        while len(links_to_visit) > 0:
+            current_page = links_to_visit.pop()
+            urls = self.search_page(current_page)
+            for url in urls:
+                if url not in visited_urls:
+                    visited_urls.add(url)
+
+        self.links = visited_urls
+        return self.links
+
+    def search_page(self, url):
+        # get all links on page at url
+        print 'Finding links at %s' % url
+        page_links = self.extract_page_links(url)
+        cleaned_links = self.clean_links(page_links)
+        links_on_page = set(cleaned_links)
+
+        return links_on_page
+
+    def extract_page_links(self, url):
+        # use bs4 to extract all links from the page
+        html_string = self.get_page(url)
+        soup = bs4.BeautifulSoup(html_string)
+        link_list = soup.find_all('a')
+        return link_list
+
+    @staticmethod
+    def get_page(url):
+        # returns html string from url
+        try:
+            data = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            raise BadURL('Unable to connect to the given URL.  Check that you typed it correctly.')
+        return data.text
+
+    def clean_links(self, html_link_list):
+        # normalizes URLs and removes external links
+        relevant_links = []
+        link_targets = [html_link_list[i].get('href') for i in range(len(html_link_list))]
+        for target in link_targets:
+            if target and len(target) > 0:
+
+                # convert relative path to absolute
+                # TODO account for internal links that don't begin with /
+                if target[0] == '/':
+                    target = self.parsed_url.scheme + '://' + self.parsed_url.netloc + target
+
+                # check if link is internal (same domain)
+                link_domain = '.'.join(self.parse_url(target).netloc.split('.')[-2:])
+                if self.domain == link_domain:
+
+                    # check that links is not an id, data uri, mailto, etc.
+                    if 'http' in target and 'mailto:' not in target:
+                        relevant_links.append(target)
+
+        return relevant_links
 
 
-def make_link_list(url):
-    # extracts all links from html
-    html_string = get_html_string(url)
-    soup = bs4.BeautifulSoup(html_string)
-    link_list = soup.find_all('a')
-    return link_list
-
-
-def extract_domain(base_url):
-    # extract the domain from the url
-    parsed_url = urlparse(base_url)
-    return parsed_url
-
-
-def get_all_links(base_url):
-    # find all links at base_url and and add them to the search queue
-    links_to_visit = search_new_page(str(base_url))
-
-    # keeps track of all urls that have been searched
-    visited_urls = set()
-
-    # search links recursively until all links have been searched
-    while len(links_to_visit) > 0:
-        current_page = links_to_visit.pop()
-        urls = search_new_page(current_page)
-        for url in urls:
-            if url not in visited_urls:
-                visited_urls.add(url)
-
-    print '\n\nFound %d internal links at %s.' % (len(visited_urls), base_url)
-    return visited_urls
-
-
-def search_new_page(url):
-    # get all links on page at url
-    print 'Finding links at %s' % url
-    new_page_links = make_link_list(url)
-    # parse here
-    parsed_url = extract_domain(url)
-    cleaned_new_page_links = extract_absolute_path(new_page_links, parsed_url)
-    links_on_page = set(cleaned_new_page_links)
-
-    return links_on_page
-
-
-def extract_absolute_path(raw_base_links, parsed_url):
-    # normalizes URLs and removes external links
-    cleaned_links = [raw_base_links[i].get('href') for i in range(len(raw_base_links))]
-    relevant_links = []
-    for link in cleaned_links:
-        if link and len(link) > 0:
-            # convert relative path to absolute
-            if link[0] == '/':
-                link = parsed_url.scheme + '://' + parsed_url.netloc + link
-            # check if link is internal and not an id, data uri, etc.
-            if '.'.join(parsed_url.netloc.split('.')[-2:]) in link and 'http' in link:
-                relevant_links.append(link)
-
-    return relevant_links
+class BadURL(Exception):
+    """Wraps ConnectionError from request module."""
+    pass
 
 
 if __name__ == '__main__':
@@ -76,5 +100,7 @@ if __name__ == '__main__':
         print 'Enter an absolute url after the command, e.g. python find_links.py http://www.google.com'
 
     else:
-        for number, link in enumerate(get_all_links(str(argv[1]))):
-            print number, link
+        finder = Finder(argv[1])
+        print 'Found %d internal links at %s.' % (len(finder), finder.url)
+        for number, link in enumerate(finder.links):
+            print number + 1, link
